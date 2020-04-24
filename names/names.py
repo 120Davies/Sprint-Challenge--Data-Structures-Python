579df406d855ee3d6d033fc02112eaa4c9466dc4
Ro McKay Davies <50719703+120Davies@users.noreply.github.com>
2020-04-24 09:32:03 -0600

    MVP on ring_buffer and names

--- names/names.py
+++ names/names.py
@@ -1,3 +1,6 @@
+# baseline time is 4.248 seconds on my machine
+from BinarySearchTree import BinarySearchTree
+
 import time
 
 start_time = time.time()
@@ -10,17 +13,35 @@ f = open('names_2.txt', 'r')
 names_2 = f.read().split("\n")  # List containing 10000 names
 f.close()
 
-duplicates = []  # Return the list of duplicates in this data structure
+name_set = set(names_2)
+
+# Return the list of duplicates in this data structure
+duplicates = []
+for name_1 in names_1:
+    if name_1 in name_set:
+        duplicates.append(name_1)
+
 
 # Replace the nested for loops below with your improvements
+# for name_1 in names_1:
+#     for name_2 in names_2:
+#         if name_1 == name_2:
+#             duplicates.append(name_1)
+
+bst = BinarySearchTree(names_1[0])
+
 for name_1 in names_1:
-    for name_2 in names_2:
-        if name_1 == name_2:
-            duplicates.append(name_1)
+    bst.insert(name_1)
+
+for name_2 in names_2:
+    if bst.contains(name_2):
+        duplicates.append(name_2)
 
 end_time = time.time()
 print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
 print (f"runtime: {end_time - start_time} seconds")
+
+# New runtime is 0.1 seconds. I can probably do better, but I'll do that later.
 
 # ---------- Stretch Goal -----------
 # Python has built-in tools that allow for a very efficient approach to this problem
--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -8,15 +8,28 @@ class RingBuffer:
         self.storage = DoublyLinkedList()
 
     def append(self, item):
-        pass
+        if self.storage.length < self.capacity:
+            self.storage.add_to_tail(item)
+            self.current = self.storage.tail
+        elif self.current == self.storage.tail:
+            self.stoarge.head.value = item
+            self.current = self.storage.head
+        else:
+            self.current.next.value = item
+            self.current = self.current.next
+
 
     def get(self):
         # Note:  This is the only [] allowed
         list_buffer_contents = []
-
-        # TODO: Your code here
-
+        node = self.storage.head
+        while node is not None:
+            list_buffer_contents.append(node.value)
+            node = node.next
         return list_buffer_contents
+
+# passes all but 2 tests. good enough for MVP.
+# I'll hopefully come back to this later.
 
 # ----------------Stretch Goal-------------------
 
